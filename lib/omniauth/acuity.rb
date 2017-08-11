2f926b642fbc0b4a4caf18a321a16033b1f363c5
lionel <lionel02218@yahoo.com>
2017-08-11 23:49:45 +0800

    First Commit

---
+++ lib/omniauth/acuity.rb
@@ -0,0 +1,7 @@
+require "omniauth/acuity/version"
+
+module Omniauth
+  module Acuity
+    # Your code goes here...
+  end
+end
---
+++ lib/omniauth/acuity/version.rb
@@ -0,0 +1,5 @@
+module Omniauth
+  module Acuity
+    VERSION = "0.1.0"
+  end
+end
